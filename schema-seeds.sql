f8a347decade1a9c2d8c40accc042b9ac91a7e09
Josh Coast <josh@joshcoast.com>
2018-05-22 10:26:49 -0600

    Start using handlebars

---
+++ schema-seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO Stories (title, body) VALUES ('A Title', "Just some stuff to talk about.");
+INSERT INTO Stories (title, body) VALUES ('Great Story Title', "Other stuff in a great story.");
+INSERT INTO Stories (title, body) VALUES ('The People', "Blue things are great! Yellow things smell bad.");
+INSERT INTO Stories (title, body) VALUES ('TomTom', "I like to watch tv.");
+INSERT INTO Stories (title, body) VALUES ('Moss', "I need to mow the lawn.");
+
