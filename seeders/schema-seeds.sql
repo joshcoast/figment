c3a6012ad52e53ef8e8af48d89f2fcb68f498539
Josh Coast <josh@joshcoast.com>
2018-05-23 21:53:11 -0600

    Adds a couple routs

--- seeders/schema-seeds.sql
+++ seeders/schema-seeds.sql
@@ -5,3 +5,8 @@ INSERT INTO Stories (title, body) VALUES ('The People', "Blue things are great! 
 INSERT INTO Stories (title, body) VALUES ('TomTom', "I like to watch tv.");
 INSERT INTO Stories (title, body) VALUES ('Moss', "I need to mow the lawn.");
 
+
+INSERT INTO Authors (name) VALUES ("Your Mom");
+INSERT INTO Authors (name) VALUES ("Your Dad");
+INSERT INTO Authors (name) VALUES ("Dave Eggers");
+INSERT INTO Authors (name) VALUES ("J.K. Rowling");
