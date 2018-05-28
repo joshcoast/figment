36b28712d053bbf44bde59528d8041b46c4060c2
Josh Coast <josh@joshcoast.com>
2018-05-27 19:10:19 -0600

    Baby steps
    Got it to write at lest something to the data base when you submit a story.

--- public/js/write.js
+++ public/js/write.js
@@ -8,6 +8,27 @@ $(document).ready(function() {
 
   console.log(currentUser);
 
+ let storyData = {
+   title: currentUser,
+   genre: "default genre",
+   description: "default description",
+   body: "default body"
+  };
+
+
+  $("#submit").on("click", function(event) {
+    $.post("/api/story-index", storyData)
+      .then(getAuthors);
+  });
+
+  function getAuthors(){
+    console.log("ran");
+  }
+
+
+
+
+
 //   // Submits a new story and brings user to story-index page upon completion
 //   function submitPost(story) {
 //     $.post("/api/story-index", story, function() {
