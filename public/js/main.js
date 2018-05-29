bc7436a74ee26c752c27e3943da41a676034da2e
unknown <you@example.com>
2018-05-28 18:30:04 -0600

    navigation

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
+
     $(".choiceimg").hover(    
     function(){
         $(".mainbody").css({
@@ -15,4 +17,25 @@ $(document).ready(function(){
             'transition': 'background 0.2s ease-in-out',
          });
     });
+    
+
+    function addmenulinks(){
+        let path = window.location.pathname;
+        if (path === "/signin"){
+            $(".menulinks").append(`<li><a href="/logout" id="logoutBtn" class="transparent btn-small">Log Out</a><li/>`)
+            $(".mobilelinks").append(`<li><a href="/logout" id="logoutBtn" >Log Out</a><li/>`)
+        } else if (path === "/read"){
+            $(".menulinks").append(`<li><a href="/write" class="transparent btn-small">Write</a><li/>
+            <li><a href="/logout" id="logoutBtn" class="transparent btn-small">Log Out</a><li/>`)
+        } else if (path === "/choice"){
+            $(".menulinks").append(`<li><a href="/logout" id="logoutBtn" class="transparent btn-small">Log Out</a><li/>`)
+        } else if (path === "/write"){
+            $(".menulinks").append(`<li><a href="/read" class="transparent btn-small">Read</a><li/>
+            <li><a href="/logout" id="logoutBtn" class="transparent btn-small">Log Out</a><li/>`)
+        }  else if (path === "/"){
+            $(".menulinks").append(`<li><a href="/signin" id="loginBtn" class="transparent btn-small">Log In</a><li/>`)
+        }
+
+    }
+    addmenulinks()
   });
