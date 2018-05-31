df751406a92f5d454539c387da138dbe11745645
Patrick East <patrick.east@icloud.com>
2018-05-31 13:06:19 -0600

    Text scroll on homepage is now working. Need to get delete in there.

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,63 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
+
+    var i = 0;
+    var txt = 'Figment.observer'; /* The text */
+    var speed = 200; /* The speed/duration of the effect in milliseconds */
+
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("textScroll").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+
     $('.sidenav').sidenav();
 
-    $(".choiceimg").hover(    
-    function(){
-        $(".mainbody").css({
-            'background' : 'linear-gradient(#206188 0% , #72c4cc 100%)',
-            '-webkit-transition': 'background 0.2s ease-in-out',
-            'transition': 'background 0.2s ease-in-out',
-         });
-    },
-    function(){
-        $(".mainbody").css({
-            'background' : 'linear-gradient(#206188 32.65% , #72c4cc 100%)',
-            '-webkit-transition': 'background 0.2s ease-in-out',
-            'transition': 'background 0.2s ease-in-out',
-         });
-    });
-    
+    $(".choiceimg").hover(
+        function () {
+            $(".mainbody").css({
+                'background': 'linear-gradient(#206188 0% , #72c4cc 100%)',
+                '-webkit-transition': 'background 0.2s ease-in-out',
+                'transition': 'background 0.2s ease-in-out',
+            });
+        },
+        function () {
+            $(".mainbody").css({
+                'background': 'linear-gradient(#206188 32.65% , #72c4cc 100%)',
+                '-webkit-transition': 'background 0.2s ease-in-out',
+                'transition': 'background 0.2s ease-in-out',
+            });
+        });
 
-    function addmenulinks(){
+
+    function addmenulinks() {
         let path = window.location.pathname;
-        if (path === "/signin"){
+        if (path === "/signin") {
             $(".menulinks").append(`<li><a href="/logout" id="logoutBtn" class="light-blue darken-3 btn-small">Log Out</a><li/>`)
             $(".mobilelinks").append(`<li><a href="/logout" id="logoutBtn" >Log Out</a><li/>`)
-        } else if (path === "/read"){
+        } else if (path === "/read") {
             $(".menulinks").append(`<li><a href="/write" class="light-blue darken-3 btn-small">Write</a><li/>
             <li><a href="/logout" id="logoutBtn" class="light-blue darken-3 btn-small">Log Out</a><li/>`)
             $(".mobilelinks").append(`<li><a href="/write">write</a><li/>
             <li><a href="/logout" id="logoutBtn">Log Out</a><li/>`)
-        } else if (path === "/choice"){
+        } else if (path === "/choice") {
             $(".menulinks").append(`<li><a href="/logout" id="logoutBtn" class="light-blue darken-3 btn-small">Log Out</a><li/>`)
             $(".mobilelinks").append(`<li><a href="/logout" id="logoutBtn" >Log Out</a><li/>`)
-        } else if (path === "/write"){
+        } else if (path === "/write") {
             $(".menulinks").append(`<li><a href="/read" class="light-blue darken-3 btn-small">Read</a><li/>
             <li><a href="/logout" id="logoutBtn" class="light-blue darken-3 btn-small">Log Out</a><li/>`)
             $(".mobilelinks").append(`<li><a href="/read" >Read</a><li/>
             <li><a href="/logout" id="logoutBtn">Log Out</a><li/>`)
-        }  else if (path === "/"){
+        } else if (path === "/") {
             $(".menulinks").append(`<li><a href="/signin" id="loginBtn" class="light-blue darken-3 btn-small">Log In</a><li/>`)
             $(".mobilelinks").append(`<li><a href="/signin" id="loginBtn"" >Log In</a><li/>`)
         }
 
     }
-    addmenulinks()
+    addmenulinks();
+    typeWriter();
 
-
-  });
+});
